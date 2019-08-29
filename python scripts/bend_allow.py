e615cebc801e3952e864446b14aa05a20ba3e0b6
0rgis <31127560+0rgis@users.noreply.github.com>
2019-08-29 20:08:38 +0100

    Update bend_allow.py

--- python scripts/bend_allow.py
+++ python scripts/bend_allow.py
@@ -3,16 +3,16 @@
 ###result = float(num1) * float(num2)
 ###print (result)
  
-###metal = input("Enter Material type: ")
-###thick = float(input("Enter material thickness: "))
-###allow = .447
-###result = (thick * allow) * 2
-###print (result)
+metal = input("Enter Material type: ")
+thick = float(input("Enter material thickness: "))
+allow = .447
+result = (thick * allow) * 2
+print (result)
 
 #start hole to fold edge script
 
-thk = float(input("Enter mtl thkness:  "))
-rad = float(input("Enter bend rad:  "))
-result = (2 * thk) + rad
-print("Closest to fold for hole edge is:  " + str(result) + "mm")
-print("0rg1sm r00lz")
+###thk = float(input("Enter mtl thkness:  "))
+###rad = float(input("Enter bend rad:  "))
+###result = (2 * thk) + rad
+###print("Closest to fold for hole edge is:  " + str(result) + "mm")
+###print("0rg1sm r00lz")
