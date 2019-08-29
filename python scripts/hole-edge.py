93e40c4e88bba13d7339733a103f47f4964a147b
0rgis <31127560+0rgis@users.noreply.github.com>
2019-08-29 19:49:43 +0100

    Update hole-edge.py

--- python scripts/hole-edge.py
+++ python scripts/hole-edge.py
@@ -1,10 +1,30 @@
-#Hole distance
-thk = float(input("Enter Material thickness:  "))
-rad = float(input("Enter Bend Radius:  "))
-hole = (2 * thk) + rad  #formula for 1.0mm or less
-slot = (2.5 * thk) + rad
-if thk <= 1:
-    print("Closest to fold for hole edge is:  " + str(hole) + "mm")
-else:
-    print("Closest to fold for hole edge is:  " + str(slot) + "mm")
-    print("Minimum flange height is: " + str(slot) + "mm")
+#python3.7
+#0rg1s pwns th1s
+#Hole/Flange distance calculator
+
+def holedge():
+    thk = float(input("Enter Material thickness:  "))
+    rad = float(input("Enter Bend Radius:  "))
+    hole = (2 * thk) + rad  #formula for 1.0mm or less
+    slot = (2.5 * thk) + rad  #formula for over 1.0 mm
+    if thk <= 1:
+        print("")
+        print("")
+        print("Closest to fold for hole edge is:  " + str(hole) + "mm")
+        print("")
+        stop = int(input("Enter 1 To Continue Or 2 To Quit: "))
+    else:
+        print("")
+        print("")
+        print("Minimum flange height & Closest to fold for hole edge is:  " + str(slot) + "mm")
+        print("")
+        print("")
+        stop = int(input("Enter 1 To Continue Or 2 To Quit: "))
+        print("")        
+    while stop == (1):
+        holedge()
+        break
+    else:
+        print("")
+        print("Good Bye ....")
+holedge()
