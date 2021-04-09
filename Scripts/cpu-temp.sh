bdfa4019ce760a7f5ad35e88878b58348c676761
0rgis <31127560+0rgis@users.noreply.github.com>
2021-04-09 23:01:41 +0100

    Update cpu-temp.sh

--- Scripts/cpu-temp.sh
+++ Scripts/cpu-temp.sh
@@ -2,3 +2,4 @@ cpu=$(cat /sys/class/thermal/thermal_zone0/temp)
 echo "$(date) @ $(hostname) $(uname -r)"
 echo "-------------------------------"
 echo "CPU => $((cpu/1000))'C"
+echo "-------------------------------"
