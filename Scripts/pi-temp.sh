131c1459bce3216f72293201e2953835a66fcdb8
0rgis <31127560+0rgis@users.noreply.github.com>
2021-04-09 21:59:33 +0100

    Update pi-temp.sh
    added -r to uname

--- Scripts/pi-temp.sh
+++ Scripts/pi-temp.sh
@@ -1,4 +1,4 @@
 cpu=$(</sys/class/thermal/thermal_zone0/temp)
-echo "$(date) @ $(hostname) $(uname)"
+echo "$(date) @ $(hostname) $(uname -r)"
 echo "-------------------------------"
 echo "CPU => $((cpu/1000))'C"
