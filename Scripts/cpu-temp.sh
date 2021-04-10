41409a21b22b990ca0ac73d472432beb42d793a0
0rgis <31127560+0rgis@users.noreply.github.com>
2021-04-10 10:18:23 -0400

    Update cpu-temp.sh
    added color

--- Scripts/cpu-temp.sh
+++ Scripts/cpu-temp.sh
@@ -1,5 +1,5 @@
 cpu=$(cat /sys/class/thermal/thermal_zone0/temp)
 echo "$(date) @ $(hostname) $(uname -r)"
 echo "-------------------------------"
-echo "CPU => $((cpu/1000))'C"
+echo -e "\e[38;5;82mCPU => $((cpu/1000))'C\e[0m"
 echo "-------------------------------"
