2ddd3664690715296ea0f3937459c3d46ab1625d
0rgis <31127560+0rgis@users.noreply.github.com>
2022-06-13 20:41:59 +0100

    Update cpu-temp.sh

--- Scripts/cpu-temp.sh
+++ Scripts/cpu-temp.sh
@@ -1,5 +1,5 @@
 cpu=$(cat /sys/class/thermal/thermal_zone0/temp)
-echo "$(date) @ $(hostname) $(uname -r)"
-echo "-------------------------------"
-echo -e "\e[38;5;82mCPU => $((cpu/1000))'C\e[0m"
-echo "-------------------------------"
+echo "\e[33m $(date) @ $(hostname) $(uname -r)\e[0m"
+echo "\e[34m-------------------------------\e[0m"
+echo "\e[38;5;82mCPU => $((cpu/1000))'C\e[0m"
+echo "\e[34m-------------------------------"
